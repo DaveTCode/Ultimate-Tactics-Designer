416b7c4a82a7b3e801d74f4fd4fa720d8c1dba13
David Tyler <davet.code@gmail.com>
2012-08-21 12:38:40 +0100

    Building test release

--- ultimatetactics/Properties/Settings.Designer.cs
+++ ultimatetactics/Properties/Settings.Designer.cs
@@ -151,7 +151,7 @@ namespace UltimateTacticsDesigner.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.0.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.1.0")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
@@ -169,7 +169,7 @@ namespace UltimateTacticsDesigner.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.9.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.1.0")]
         public string UsersVersion {
             get {
                 return ((string)(this["UsersVersion"]));
